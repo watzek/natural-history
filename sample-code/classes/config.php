b5d4c8b0295ecc228d119610fdf654e3a0fc11e2
Jeremy McWilliams <jeremym@lclark.edu>
2019-06-03 14:30:28 -0700

    examples for OOP in PHP

---
+++ sample-code/classes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$server="";
+$dbname="";
+$user="";
+$password="";
+
+
+
+
+
+
+
+ ?>
---
+++ sample-code/classes/main.class.php
@@ -0,0 +1,53 @@
+<?php
+include ("templates.class.php");
+
+
+/* the "main" class is sort of the switchboard for changing views within the page.
+It relies on the templates class (in templates.class.php) for
+
+*/
+
+class main{
+
+
+  /* This is a PHP convention - the __construct function gets run every time the class is initiated    */
+
+  /* The variable $state is passed to it when the class is called from index.php */
+  function __construct(){
+
+    /**/
+    $templates=new templates();
+    $this->templates=templates;
+
+  }
+
+  function getView($state){
+
+    switch($state){
+
+      case "home":
+      $templates->home();
+      break;
+
+      case "about":
+      $templates->about();
+      break;
+
+      case "collection":
+      $templates->collection($_GET["collectionID"]);
+      break;
+
+    }
+
+
+
+
+
+  }
+
+
+
+
+}
+
+?>
